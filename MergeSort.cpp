a84a939185ec5becf6e19008d456cf6f74a096f2
NurulNajatMony <nurulmony505@gmail.com>
2025-04-17 11:08:37 +0700

    mergeSort step 2

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -16,3 +16,15 @@ void input() {
             else {
                 cout << "\nMaksimal panjang array adalah 20";
             }
+        }
+
+        cout << "\n================="
+        cout << "\nInputkan Isi Element Array";
+        cout << "\n=================" << endl;
+    
+        for (int i = 0;i < n; i++ ) {
+            cout << "Array index ke - " << i << ": ";
+            cin >> arr[i];
+        }
+    }
+    
