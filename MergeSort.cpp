9b5f45976797d62462eef3b45e18a65372829f84
NurulNajatMony <nurulmony505@gmail.com>
2025-04-17 11:13:30 +0700

    menambahkan input for

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -56,6 +56,17 @@ void input() {
     
             k++;
         }
+        
+        while (j <= high) {     //step 4.e
+            B[k] = arr[j];      //step 4.e.i
+            j++;                //step 4.e.ii
+            k++;                //step 4.e.iii
+        }
+    
+        while (i <= mid) {     //step 4.f
+            B[k] = arr[i];     //step 4.f.i
+            i++;               //step 4.f.ii
+            k++;               //step 4.f.iii
     
     
     
