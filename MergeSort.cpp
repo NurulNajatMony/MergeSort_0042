4756fed337bd730368b0a68218f5bd9ced133e09
NurulNajatMony <nurulmony505@gmail.com>
2025-04-17 11:06:43 +0700

    menginput isi element array

---
+++ MergeSort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int arr[20], B[20];
+
+int n;
+
+void input() {
+        while (true){
+            cout << "Masukkan panjang element array: ";
+            cin >> n;
+
+            if (n<= 20) {
+                break;
+            }
+            else {
+                cout << "\nMaksimal panjang array adalah 20";
+            }
